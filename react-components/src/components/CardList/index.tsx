2ac59ca4f157ec82a73648c20f3361bb3216997b
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2022-10-17 19:44:12 +0300

    feat: add predicate function

--- react-components/src/components/CardList/index.tsx
+++ react-components/src/components/CardList/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './index.css';
 import { IProps } from 'components/CardList/interfaces';
+import { ICaracter } from 'pages/Home/interfaces';
+import { IFormCards } from 'types';
 import Card from 'components/Card';
+import FormCard from 'components/FormCard';
 
-function CardList({ data }: IProps) {
+const isType = (data: ICaracter | IFormCards): data is ICaracter => {
+  return 'species' in data;
+};
+
+const CardList = ({ data }: IProps) => {
   return (
     <ul className="card-list" role="card-list">
       {data.map((card, i) => (
         <li key={i}>
-          <Card key={i} data={card} />
+          {isType(card) ? <Card key={i} data={card} /> : <FormCard key={i} data={card} />}
         </li>
       ))}
     </ul>
   );
-}
+};
 
 export default CardList;
--- react-components/src/components/CardList/interfaces.tsx
+++ react-components/src/components/CardList/interfaces.tsx
@@ -1,5 +1,6 @@
 import { ICaracter } from 'pages/Home/interfaces';
+import { IFormCards } from 'types';
 
 export interface IProps {
-  data: ICaracter[];
+  data: ICaracter[] | IFormCards[];
 }
