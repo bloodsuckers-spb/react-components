9487997502b5366894a9d4a1249b6bb43da23029
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2022-10-18 17:40:25 +0300

    feat: add modal close btn

--- react-components/src/components/Modal/index.tsx
+++ react-components/src/components/Modal/index.tsx
@@ -4,10 +4,14 @@ import { IProps } from './interfaces';
 
 const Modal = ({ handler, card }: IProps) => {
   const { name, image, gender, species } = card;
+  const handleClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
   return (
     <div className="modal" onClick={handler}>
       <div className="overlay">
-        <div className="modal-window">
+        <div className="modal-window" onClick={handleClick}>
+          <div className="modal-close-icon" onClick={handler}></div>
           <img className="card-img" src={image} />
           <h4>{name}</h4>
           <p className="gender">{gender}</p>
